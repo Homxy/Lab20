de1a71e291381d7e96a65da8a928eda09ea1eb02
Homxy <120322865+Homxy@users.noreply.github.com>
2023-02-27 18:49:42 +0700

    Update lab20_1.cpp

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -5,8 +5,18 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect *r1, Rect *r2){
+    double p1=max(r1->x, r2->x);
+    double p2=min(r1->x+r1->w, r2->x+r2->w);
+    double p3=max(r1->y-r1->h, r2->y-r2->h);
+    double p4=min(r1->y, r2->y);
+    
+    double area = (p2-p1)*(p4-p3);
+    
+    if(area>0){
+        return area;
+    }
+    else{
+        return 0;
+    }
 }
